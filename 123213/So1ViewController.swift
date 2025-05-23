468b2ac9081b7fa8654cc0ab1dcb1de98f37cdf5
Qduk22 <146013823+Qduk22@users.noreply.github.com>
2025-05-23 22:47:50 +0700

    add so 1

---
+++ 123213/So1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  So1ViewController.swift
+//  123213
+//
+//  Created by Dukkk on 23/05/2025.
+//
+
+import UIKit
+
+class So1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("123")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
